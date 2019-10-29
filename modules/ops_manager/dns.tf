a5e0b0892bb99207358da364d6315de4104414bc
warroyo <warroyo7199008@gmail.com>
2019-10-28 18:00:39 -0600

    account for private opsman

--- modules/ops_manager/dns.tf
+++ modules/ops_manager/dns.tf
@@ -13,9 +13,9 @@ resource "aws_route53_record" "ops_manager_unattached_eip" {
   zone_id = "${var.zone_id}"
   type    = "A"
   ttl     = 300
-  count   = "${var.use_route53 && (local.ops_man_vm < 1) ? 1 : 0}"
+  count   = "${var.use_route53 && (local.ops_man_vm < 1) && (var.private != true) ? 1 : 0}"
 
-  records = ["${coalesce(join("", aws_eip.ops_manager_attached.*.public_ip), aws_instance.ops_manager.private_ip)}"]
+  records = ["${aws_eip.ops_manager_unattached.*.public_ip}"]
 }
 
 resource "aws_route53_record" "optional_ops_manager" {
